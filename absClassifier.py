217c7df3c7d3b9efa8c91bd8e8651c4fe4d3ed7f
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-11-13 21:25:21 +0100

    write neural weights in json

--- absClassifier.py
+++ absClassifier.py
@@ -6,22 +6,32 @@
 import json
 import numpy
 
-w = [0,0]
+w = [0,0]                                       # list of the weights of the neural network
 
-def load_json(name):
+def load_json(name):                            # loads json data
     f = open("data_absClassifier.json", "r")
     data = json.load(f)
-    if name == "all":
-        return data
+    f.close()
     return data[name]
 
-def write_json(dict):
-    with open("data_absClassifier.json","w") as json_file:
-        oldFile = load_json("all")
-        z = json.loads(oldFile)
-        z.update(dict)
-        json.dump(z, "data_absClassifier.json")
-def train_neural_network():
+def estetics_in_json(string):                   # makes the json readable before upload
+    string = string.replace("'",'"')
+    string = string.replace("],","],\n")
+    string = string.replace("{","{\n")
+    string = string.replace("}", "\n}")
+    string = string.replace(', "',',\n"')
+    return string
+
+def write_json(data):                           # updates the json data with the neural_network configuration
+    training_data = load_json("training_data")
+    to_classify_data = load_json("to_classify")
+    new_dict = {"training_data": training_data, "neural_network_configuration": data, "to_classify": to_classify_data}
+
+    json_file = open("data_absClassifier.json", "w")
+    json_file.write(estetics_in_json(str(new_dict)))
+    json_file.close()
+
+def train_neural_network():                     # def to find adapt the weights due to mathematical greadients
 
     # define network variabel
     training_data = load_json("training_data")
@@ -39,7 +49,14 @@ def train_neural_network():
             w[0] += error*etha*x1[e]
             w[1] += error*etha*x2[e]
 
+def classify_new_data(json_object):
+    data = load_json(json_object)
+    print(data)
+
 train_neural_network()
-write_json({"neural_network_configuration": {"w": w}})
 print("w1: "+ str(w[0]))
 print("w2: "+ str(w[1]))
+
+write_json({"w": w})
+
+classify_new_data("to_classify")
