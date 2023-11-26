6092c22f7841e149735fcc414e37c14d8583b5cd
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-11-26 21:36:50 +0100

    dosent work anyway...

--- hidden_neuron_network/main.py
+++ hidden_neuron_network/main.py
@@ -1,40 +1,38 @@
 from neurons import neuron
-import random
-import matplotlib
-w = [
-    [1,1],
-    [random.random(), random.random()],
-    [random.random(), random.random()],
-    [random.random()],
-    [random.random()]
-]
+import copy
 
-etha = 0.01
+w = [[float(1),float(1)], [float(1), float(1)], [float(1), float(1)], [float(1)], [float(1)]]
+
+etha = 1
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-
+for a in range(len(x)):
+    x[a] = float(x[a])
+    y[a] = float(y[a])
 
 # loop for training index
 for running_index in range(100):
 
     # loop for length of input
     for i in range(len(x)):
-        neuron1 = neuron(x[i],[w[0][0]])
-        neuron2 = neuron([1],w[0][1])
-        neuron3 = neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
-        neuron4 = neuron([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
-        neuron5 = neuron([neuron3.y, neuron4.y], [w[3], w[4]])
+        neuron1 = copy.deepcopy(neuron([x[i]],[w[1][0]]))
+        neuron2 = copy.deepcopy(neuron([1],[w[1][1]]))
+        neuron3 = copy.deepcopy(neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]]))
+        neuron4 = copy.deepcopy(neuron([neuron1.y, neuron2.y], [w[1][1], w[2][1]]))
+        neuron5 = copy.deepcopy(neuron([neuron3.y, neuron4.y], [w[3], w[4]]))
         e = y[i] - neuron5.y
-        w[1][0] += 2*e*neuron3.z*(1-neuron3.z)*neuron1.y*etha
-        w[1][1] += 2*e*neuron4.z*(1-neuron4.z)*neuron1.y*etha
-        w[2][0] += 2*e*neuron3.z*(1-neuron3.z)*neuron2.y*etha
-        w[2][1] += 2*e*neuron4.z*(1-neuron4.z)*neuron2.y*etha
-        w[4] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron4.y *etha
-        w[3] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron3.y *etha
-
+        #print(e)
+        w[3] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron3.y * etha
+        w[4] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron4.y * etha
+        w[1][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3] * neuron3.z * (1 - neuron3.z) * neuron1.y * etha
+        w[1][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4] * neuron4.z * (1 - neuron4.z) * neuron1.y * etha
+        w[2][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3] * neuron3.z * (1 - neuron3.z) * neuron2.y * etha
+        w[2][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4] * neuron4.z * (1 - neuron4.z) * neuron2.y * etha
+        #print(w[3])
+        #print(str(w[1][0]) + "\n")
 new_y = []
 
-for i in range (len(x)):
+for i in range(len(x)):
     neuron1 = neuron(x[i], [w[0][0]])
     neuron2 = neuron([1], w[0][1])
     neuron3 = neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
@@ -42,6 +40,7 @@ for i in range (len(x)):
     neuron5 = neuron([neuron3.y, neuron4.y], [w[3], w[4]])
     new_y.append(neuron5.y)
 
+print("--------------------------------\n\n\n")
 print(x)
 print(new_y)
 
